eda81ab3ac0c5f41c296ef576a4a9f3e10f2acdd
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-07-20 11:54:39 +0530

    #include<iostream> using namespace std; int main() {     int n;     cin>>n;     {         if(n%9==0)         {             cout<<"True";         }         else         {             cout<<"False";         }     }     return 0; }

---
+++ Number_Divisible_with_9.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    {
+        if(n%9==0)
+        {
+            cout<<"True";
+        }
+        else
+        {
+            cout<<"False";
+        }
+    }
+    return 0;
+}
