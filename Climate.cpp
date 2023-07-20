fb7e6112c1ce9d3cad9879c6cf423f8ef6c837a0
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-07-20 11:57:57 +0530

    #include<iostream> using namespace std; int main() {     int n;     cin>>n;     if(n>20)     {         cout<<"HOT";     }     else     {         cout<<"COLD";     }     return 0; }

---
+++ Climate.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>20)
+    {
+        cout<<"HOT";
+    }
+    else
+    {
+        cout<<"COLD";
+    }
+    return 0;
+}
