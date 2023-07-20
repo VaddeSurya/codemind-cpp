7c5fe5694ba545be4b4e2efc8504995cdaceab3d
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-07-20 12:04:55 +0530

    #include<iostream> using namespace std; int main() {     int n,m;     cin>>n>>m;     {         if(n>=m*30)         {             cout<<"YES";         }         else         {             cout<<"NO";         }     } }

---
+++ Expenditure.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    {
+        if(n>=m*30)
+        {
+            cout<<"YES";
+        }
+        else
+        {
+            cout<<"NO";
+        }
+    }
+}
