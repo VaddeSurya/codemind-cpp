f62e476290802a6a1b3907fa727d83fd1e802f2a
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-07-20 12:08:53 +0530

    #include<iostream> using namespace std; int main() {     int x,a,b;     cin>>x>>a>>b;     {         if(x<=(1*a)+(2*b))         {             cout<<"Qualify";         }         else         {             cout<<"Not Qualify";         }     } }

---
+++ Contest:.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,a,b;
+    cin>>x>>a>>b;
+    {
+        if(x<=(1*a)+(2*b))
+        {
+            cout<<"Qualify";
+        }
+        else
+        {
+            cout<<"Not Qualify";
+        }
+    }
+}
