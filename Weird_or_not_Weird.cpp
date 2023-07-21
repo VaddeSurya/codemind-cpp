59946a503d1149129343396753a4654d4e841166
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-07-21 11:31:48 +0530

    #include<iostream> using namespace std; int main() {     int n;     cin>>n;     if(n%2!=0)     {         cout<<"weird";     }     else if(n%2 && 2,4)     {         cout<<"not weird";     }     else if(n%2 && 6,8,10,12,14,16,18,20)     {         cout<<"weird";     }     else if(n%2 && n>=20)     {         cout<<"not weird";     }     return 0; }

---
+++ Weird_or_not_Weird.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2!=0)
+    {
+        cout<<"weird";
+    }
+    else if(n%2 && 2,4)
+    {
+        cout<<"not weird";
+    }
+    else if(n%2 && 6,8,10,12,14,16,18,20)
+    {
+        cout<<"weird";
+    }
+    else if(n%2 && n>=20)
+    {
+        cout<<"not weird";
+    }
+    return 0;
+}
