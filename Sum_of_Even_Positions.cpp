6c9a5453a5c691eeba92085396f8170b0bf4de93
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-05 18:51:59 +0530

    #include <iostream>
    using namespace std;
    
    int main() {
        int n;
        cin >> n;
        int arr[n];
        for (int i = 0; i < n; ++i) {
            cin >> arr[i];
        }
        int even = 0;
    
        for (int i = 0; i < n; i += 2) {
            even += arr[i];
        }
    
        cout << even << endl;
    
        return 0;
    }

---
+++ Sum_of_Even_Positions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    int even = 0;
+
+    for (int i = 0; i < n; i += 2) {
+        even += arr[i];
+    }
+
+    cout << even << endl;
+
+    return 0;
+}
