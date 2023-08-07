e1536f77519135aabca96a192a5c50e7195eeb7a
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-07 15:37:32 +0530

    #include<iostream> using namespace std; int main() {     int n,dig,rev=0;     cin>>n;     while(n>0)     {         dig=n%10;         rev=(rev*10)+dig;         n/=10;     }     cout<<rev; }

--- reverse_of_a_numbers.cpp
+++ reverse_of_a_numbers.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main()
 {
-    int n,rev=0;
+    int n,dig,rev=0;
     cin>>n;
     while(n>0)
     {
-       rev=rev*10+n%10;
-       n/=10;
+        dig=n%10;
+        rev=(rev*10)+dig;
+        n/=10;
     }
     cout<<rev;
 }
