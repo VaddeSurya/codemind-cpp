34e420c0fa2ff365fbe88b3047a9a6fd0c890192
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-07 10:14:47 +0530

    #include<iostream> using namespace std; int main() {     int n,rev=0,dig;     cin>>n;     int m=n;     while(n!=0)     {         dig=n%10;         rev=(rev*10)+dig;         n/=10;     }     if(rev==m)     {         cout<<"True";     }     else     {         cout<<"False";     } }

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main()
 {
-     int n, num, digit, rev = 0;
-     cin >> num;
-     n = num;
-     do
-     {
-         digit = num % 10;
-         rev = (rev * 10) + digit;
-         num = num / 10;
-     } while (num != 0);
-     if (n == rev)
-         cout << "True";
-     else
-         cout << "False";
-
-    return 0;
+    int n,rev=0,dig;
+    cin>>n;
+    int m=n;
+    while(n!=0)
+    {
+        dig=n%10;
+        rev=(rev*10)+dig;
+        n/=10;
+    }
+    if(rev==m)
+    {
+        cout<<"True";
+    }
+    else
+    {
+        cout<<"False";
+    }
 }
