ae73a2ffac91a0742bd33d904c6e64fc08c5b2ea
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-07 15:58:07 +0530

    #include<iostream> using namespace std; int main() {     int n,dig,lag=0;     cin>>n;     while(n>0)     {         dig=n%10;         if(dig>lag)         {             lag=dig;         }         n/=10;     }     cout<<lag;
    }

--- Largest_Digit.cpp
+++ Largest_Digit.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 int main()
 {
-	int n, digit, large = 0;
-	cin >> n;
-	while (n > 0)
-	{
-		digit = n % 10;
-		if (digit > large)
-		{
-			large = digit;
-		}
-		n = n / 10;
-	}
-	cout << large;
+    int n,dig,lag=0;
+    cin>>n;
+    while(n>0)
+    {
+        dig=n%10;
+        if(dig>lag)
+        {
+            lag=dig;
+        }
+        n/=10;
+    }
+    cout<<lag;
+    
 }
