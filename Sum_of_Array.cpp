819be42c7aec20b6dd0d314b5117f5f1b5ef54a2
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-08 10:50:50 +0530

    #include<iostream> using namespace std; int main() {     int n,arr[100],i,sum=0;     cin>>n;     for(i=0;i<n;i++)     {         cin>>arr[i];     }     for(i=0;i<n;i++)     {         sum+=arr[i];     }     cout<<sum; }

--- Sum_of_Array.cpp
+++ Sum_of_Array.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
+int main()
+{
+    int n,arr[100],i,sum=0;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
     }
-    int sum = 0;
-    for (int i = 0; i < n; ++i) {
-            sum += arr[i];
-        }
-    cout << sum << endl;
-    return 0;
+    for(i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    cout<<sum;
 }
