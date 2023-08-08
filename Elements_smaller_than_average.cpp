c8c7db44d35d9fdf15fc0b0c60a2e6dae32385f5
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-08 21:38:11 +0530

    #include<iostream> using namespace std; int main() {     int arr[100],i,n,sum = 0,aver,count=0;     cin>>n;     for(i=0;i<n;i++)     {         cin>>arr[i];         sum += arr[i];     }     aver=sum/n;     for(i=0;i<n;i++)     {         if(arr[i]<=aver)         {             count+=1;         }     }     cout<<count; }

---
+++ Elements_smaller_than_average.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[100],i,n,sum = 0,aver,count=0;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        sum += arr[i];
+    }
+    aver=sum/n;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<=aver)
+        {
+            count+=1;
+        }
+    }
+    cout<<count;
+}
